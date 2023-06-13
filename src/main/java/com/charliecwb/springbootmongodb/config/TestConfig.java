65710bcb97899bf3c2d9f36a5a8692c567dc9616
charliecwb <charliecwb@gmail.com>
2023-06-13 15:27:41 -0300

    Database test config

---
+++ src/main/java/com/charliecwb/springbootmongodb/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.charliecwb.springbootmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.charliecwb.springbootmongodb.domain.User;
+import com.charliecwb.springbootmongodb.repositories.UserRepository;
+
+@Configuration
+public class TestConfig implements CommandLineRunner{
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com");
+		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+			
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));		
+	}	
+}
